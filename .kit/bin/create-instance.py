58cf6ce47e7aa6fbe5fcef1898e7bfa72ba1e3ee
Stoney Jackson <dr.stoney@gmail.com>
2022-01-16 16:48:48 -0500

    WIP: push main to instance repo

--- .kit/bin/create-instance.py
+++ .kit/bin/create-instance.py
@@ -3,17 +3,26 @@
 create-instance.py
 '''
 
+
+from pathlib import Path
 import sys
 
 
 import github
+import git
 
 
 def main():
+    token, org_name, repo_name = get_command_line_params()
+    create_instance_repo(token, org_name, repo_name)
+
+
+def get_command_line_params():
     cl = CommandLine()
-    gh = github.Github(cl.get_token())
-    erc = EmptyRepositoryCreator(gh)
-    erc.create(cl.get_org_name(), cl.get_repo_name())
+    token = cl.get_token()
+    org_name = cl.get_org_name()
+    repo_name = cl.get_repo_name()
+    return token, org_name, repo_name
 
 
 class CommandLine:
@@ -30,6 +39,78 @@ class CommandLine:
 
     def _get_repo_name_parts(self):
         return sys.argv[2].split('/')
+
+
+def create_instance_repo(token, org_name, repo_name):
+    InstanceCreator().create_instance(token, org_name, repo_name)
+
+
+class InstanceCreator:
+    def __init__(self):
+        self.token = None
+        self.github = None
+        self.username = None
+        self.instance_repo = None
+        self.local_repo = None
+        self.progress_monitor = MyProgressPrinter()
+
+    def create_instance(self, token, org_name, repo_name):
+        self.connect_to_github(token)
+        self.create_empty_instance_repo(org_name, repo_name)
+        self.create_instance_remote()
+        self.push_main_to_instance_repo()
+
+    def connect_to_github(self, token):
+        self.token = token
+        self.github = github.Github(self.token)
+        self.username = self.github.get_user().login
+
+    def create_empty_instance_repo(self, org_name, repo_name):
+        self.instance_repo = create_empty_repository(self.github, org_name, repo_name)
+
+    def create_instance_remote(self):
+        local_repo = self.get_local_repo()
+        auth_url = self.get_authorized_clone_url()
+        local_repo.create_remote('instance', auth_url)
+        return local_repo.remote('instance')
+
+    def get_local_repo(self):
+        if self.local_repo is None:
+            self.local_repo = get_local_repo()
+        return self.local_repo
+
+    def get_authorized_clone_url(self):
+        return self.instance_repo.clone_url.replace('https://', f'https://{self.username}:{self.token}@')
+
+    def push_main_to_instance_repo(self):
+        self.get_local_repo().remote('instance').push('main', progress=self.progress_monitor).raise_if_error()
+
+
+class MyProgressPrinter(git.RemoteProgress):
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        print(op_code, cur_count, max_count, cur_count / (max_count or 100.0), message or "NO MESSAGE")
+# end
+
+
+def get_local_repo():
+    d = Path(__file__).parent
+    while not contains_git_dir(d):
+        d = d.parent
+    return git.Repo(d)
+
+
+def contains_git_dir(d):
+    subds = [subd for subd in d.iterdir() if subd.is_dir()]
+    for subd in subds:
+        if subd.name == '.git':
+            return True
+    return False
+
+
+def create_empty_repository(gh, org_name, repo_name):
+    erc = EmptyRepositoryCreator(gh)
+    erc.create(org_name, repo_name)
+    return erc.repo
 
 
 class EmptyRepositoryCreator:
