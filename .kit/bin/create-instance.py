b587135a4b13a3d232ddf0a6bec0ad4379c01773
Stoney Jackson <dr.stoney@gmail.com>
2022-01-16 18:32:49 +0000

    WIP: refactor

--- .kit/bin/create-instance.py
+++ .kit/bin/create-instance.py
@@ -1,41 +1,55 @@
 #!/usr/bin/env python3.10
-import os
+'''
+create-instance.py
+'''
+
 import sys
-import pathlib
+
 
 import github
 
 
 def main():
-    token, full_repo_name = get_params()
-    api_repo = create_empty_repository_on_github(token, full_repo_name)
-    
+    cl = CommandLine()
+    gh = github.Github(cl.get_token())
+    erc = EmptyRepositoryCreator(gh)
+    erc.create(cl.get_org_name(), cl.get_repo_name())
 
 
-def get_params():
-    return sys.argv[1:]
+class CommandLine:
+    def get_token(self):
+        return sys.argv[1]
+
+    def get_org_name(self):
+        parts = self._get_repo_name_parts()
+        return None if len(parts) == 1 else parts[0]
+
+    def get_repo_name(self):
+        parts = self._get_repo_name_parts()
+        return parts[0] if len(parts) == 1 else parts[1]
+
+    def _get_repo_name_parts(self):
+        return sys.argv[2].split('/')
 
 
-def create_empty_repository_on_github(token, full_repo_name):
-    parts = full_repo_name.split('/')
-    if len(parts) == 1:
-        g = github.Github(token)
-        u = g.get_user()
-        return u.create_repo(parts[0])
-    else:
-        g = github.Github(token)
-        u = g.get_user()
-        o = get_org_by_login(u, parts[0])
-        return o.create_repo(parts[1])
+class EmptyRepositoryCreator:
+    def __init__(self, github):
+        self.github = github
+        self.repo = None
 
+    def create(self, org_name, repo_name):
+        namespace = self._get_namespace(org_name)
+        self.repo = namespace.create_repo(repo_name)
 
-def get_org_by_login(user, org_name):
-    orgs = user.get_orgs()
-    for o in orgs:
-        if o.login.lower() == org_name:
-            return o
-    raise Exception(f'You are not a member of {org_name}.')
+    def _get_namespace(self, org_name):
+        u = self.github.get_user()
+        return u if org_name is None else self._get_org(u, org_name)
 
+    def _get_org(self, user, org_name):
+        for o in user.get_orgs():
+            if o.login.lower() == org_name:
+                return o
+        raise Exception(f'You are not a member of {org_name}.')
 
 
 if __name__ == '__main__':
