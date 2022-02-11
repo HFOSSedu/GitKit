89c6f107b611360a87a9055b36285f66e0fb6527
Stoney Jackson <dr.stoney@gmail.com>
2022-02-11 21:31:15 +0000

    WIP

--- .kit/pylib/kit/create_instance.py
+++ .kit/pylib/kit/create_instance.py
@@ -1,4 +1,5 @@
 import pathlib
+from time import sleep
 from kit import github
 from kit import json
 from kit import git
@@ -17,6 +18,7 @@ def create_instance(token, user, repo_desc, remote_name='kit_instance', labels_f
     local_repo.create_head('main', origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
 
     # Commit upstream name to main
+    sleep(2)
     env_file_dir = pathlib.Path(local_repo.working_tree_dir) / '.kitty'
     assert env_file_dir.exists() and env_file_dir.is_dir()
     env_file = env_file_dir / 'env'
