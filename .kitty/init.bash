83a8952d1008e20c5cea8e09b410d3971378fa0f
Stoney Jackson <dr.stoney@gmail.com>
2022-02-12 11:32:53 -0500

    WIP: improve message

--- .kitty/init.bash
+++ .kitty/init.bash
@@ -17,15 +17,10 @@ if [[ "$ORIGIN" == *"$KIT_UPSTREAM_NAME"* ]] ; then
     printf "\xF0\x9F\x98\xBA Meow, Kitty here!\n"
     echo
     echo "Oops, I think you have cloned the upstream repository instead of your"
-    echo "fork. Use the following command below to inspect your local"
-    echo "repository's remotes."
+    echo "fork. But don't worry. You can fix it!"
     echo
-    echo "    git remote -v"
-    echo
-    echo "Look for the 'origin' remote. If its URL points to upstream and not"
-    echo "your fork, you have a problem. But don't worry. You can fix it!"
-    echo "You'll need to delete your local repository, then navigate to your"
-    echo "fork on GitHub, copy its clone URL, and then try clone using that"
-    echo "URL. Don't forget to initialize me again so I can help out."
+    echo "1. Delete your local repository."
+    echo "2. Navigate to your fork on GitHub and copy its clone URL."
+    echo "3. Clone your fork using its clone URL."
     echo "*********************************************************************"
 fi
